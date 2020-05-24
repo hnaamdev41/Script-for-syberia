f387242e0dbc61d0c0c2b0a5f9c57f38e2e260ea
Himanshu Naamdev <46525642+hnaamdev41@users.noreply.github.com>
2020-05-24 14:17:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,8 +13,8 @@ git clone https://github.com/hnaamdev41/audio-caf.git -b ten hardware/qcom/audio
 git clone https://github.com/hnaamdev41/media-caf.git -b ten hardware/qcom/media-caf/msm8996
 git clone https://github.com/LineageOS/android_external_ant-wireless_antradio-library.git -b lineage-17.1 external/ant-wireless/antradio-library
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-17.1 packages/resources/devicesettings
-git clone https://github.com/zeelog/android_kernel_xiaomi_mido.git -b lineage-17.1 kernel/xiaomi/mido
-git clone https://github.com/hnaamdev41/proprietary_vendor_xiaomi.git -b ten vendor/xiaomi
+git clone https://github.com/KudProject/kernel_xiaomi_msm8953-4.9.git -b lineage-17.1 kernel/xiaomi/mido
+git clone https://github.com/KudProject/proprietary_vendor_xiaomi.git -b ten vendor/xiaomi
 git clone https://github.com/TRINKET-ANDROID/platform_external_json-c.git -b 10.0 external/json-c
 git clone https://github.com/TRINKET-ANDROID/platform_prebuilts_abi-dumps_vndk.git -b 10.0 prebuilts/abi-dumps/vndk
 git clone https://github.com/TRINKET-ANDROID/platform_prebuilts_abi-dumps_ndk.git -b 10.0 prebuilts/abi-dumps/ndk
@@ -22,5 +22,4 @@ git clone https://github.com/TRINKET-ANDROID/platform_packages_apps_FMRadio.git 
 git clone https://github.com/TRINKET-ANDROID/platform_vendor_qcom_opensource_fm-commonsys.git -b 10.0 vendor/qcom/opensource/fm-commonsys
 git clone https://github.com/TRINKET-ANDROID/platform_vendor_qcom_opensource_libfmjni.git -b 10.0 vendor/qcom/opensource/libfmjni
 git clone https://github.com/TRINKET-ANDROID/platform_hardware_qcom-caf_common.git -b 10.0 hardware/qcom/common
-git clone https://github.com/SyberiaProject-Devices/platform_device_xiaomi_mido -b 10.0 device/xiaomi/mido
-. b*/e* && lunch syberia_mido-userdebug && source ~/.bashrc && export USE_CCACHE=1 && ccache -M 100G && make bacon -j30
+git clone https://github.com/hnaamdev41/device_xiaomi_mido_Q.git -b syberia_4.9 device/xiaomi/mido
